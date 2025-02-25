e36a04ac32f4e0a1455d3c99782b3f3c3f78dada
goforit615 <bupark82@gsm.hs.kr>
2025-02-25 11:48:24 +0900

    Member

---
+++ src/collection/Member.java
@@ -0,0 +1,18 @@
+package collection;
+
+public class Member {
+    private int memberId;        //회원 아이디
+    private String memberName;   //회원 이름
+
+    public Member(int memberId, String memberName){ //생성자
+        this.memberId = memberId;
+        this.memberName = memberName;
+    }
+
+    public int getMemberId() {  //
+        return memberId;
+    }
+    public void setMemberId(int memberId) {
+        this.memberId = memberId;
+    }
+}
