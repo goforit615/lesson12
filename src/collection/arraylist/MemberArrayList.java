bb9425c0c3f0e7392d645de163adebb06af12436
goforit615 <bupark82@gsm.hs.kr>
2025-02-25 12:57:52 +0900

    MemberArrayListTest

--- src/collection/arraylist/MemberArrayList.java
+++ src/collection/arraylist/MemberArrayList.java
@@ -10,6 +10,10 @@ public class MemberArrayList {
         arrayList = new ArrayList<Member>();
     }
 
+    public void addMember(Member member) {
+        arrayList.add(member);
+    }
+
     public boolean removeMember(int memberId) {
         for(int i = 0; i < arrayList.size(); i++) {
             Member member = arrayList.get(i);
